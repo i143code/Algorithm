7961354692c0664edd7cf9b68713015059550690
i143code <ranjanah143code@yahoo.com>
2016-03-05 13:59:59 -0500

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,12 +1,13 @@
 //Ashish Ranjan
 
 import java.util.*;
-//		 At every step, consider the array between low and high indices
-//		 Calculate the mid index.
-//		 If element at the mid index is the key, return mid.
-//		 If element at mid is greater than the key, then reduce the array size such that high becomes mid - 1. Index at low remains the same.
-//		 If element at mid is less than the key, then reduce the array size such that low becomes mid + 1. Index at high remains the same.
-//		 When low is greater than high, key doesn't exist. Return -1.
+//Assumed the array is already sorted
+//At every step, consider the array between low and high indices
+//Calculate the mid index.
+//If element at the mid index is the key, return mid.
+//If element at mid is greater than the key, then reduce the array size such that high becomes mid - 1. Index at low remains the same.
+//If element at mid is less than the key, then reduce the array size such that low becomes mid + 1. Index at high remains the same.
+//When low is greater than high, key doesn't exist. Return -1.
 class BinarySearch{
 	
 	public static void main(String[] args){
